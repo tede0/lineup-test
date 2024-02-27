b28ee5f5d934838623be65770a4086d263d2d559
Maksym Lyskov <admin@Maksyms-MBP.lan>
2024-02-27 11:05:12 +0000

    limit response to user data only

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,17 @@
 import httpx
 from fastapi import FastAPI, HTTPException
 
-from .models.user import User
+from .models.user import UserData
 from .services.user_data_service import fetch_user_data
 
 
 app = FastAPI()
 
 
-@app.get("/user/{user_id}", response_model=User)
-async def read_user(user_id: int):
+@app.get("/user/{user_id}", response_model=UserData)
+async def get_user_data(user_id: int):
     try:
         user_data = await fetch_user_data(user_id)
-        return user_data
+        return user_data.data
     except httpx.HTTPStatusError as e:
         raise HTTPException(status_code=e.response.status_code, detail=str(e))
